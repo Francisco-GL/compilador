97890ab6895dafb377807dad61682e2402a26135
Francisco GL <82791471+Francisco-GL@users.noreply.github.com>
2023-05-08 02:31:04 -0600

    IDE.py and lexicalAnalyzer.py updated

--- src/compiler/lexicalAnalyzer.py
+++ src/compiler/lexicalAnalyzer.py
@@ -6,57 +6,133 @@ def lexer(code):
     keywords = ['main', 'if', 'then', 'else', 'end', 'do', 'while',
                 'repeat', 'until', 'cin', 'cout', 'real', 'int', 'boolean']
     punctuations = ['(', ')', '{', '}', ',', ';']
-    operators = ['+', '-', '*', '/', '%', '<',
-                 '<=', '>', '>=', '==', '!=', ':=']
-    tokens = []
-    errors = []
+    operators = ['+=', '++', '--', '*', '/', '%', '<',
+                 '<=', '>', '>=', '==', '!=', ':=', '=', '+', '-']
 
-    # Define regex patterns
     keyword_pattern = '|'.join('\\b{}\\b'.format(kw) for kw in keywords)
     id_pattern = '[a-zA-Z][a-zA-Z0-9]*'
     int_pattern = '[0-9]+'
-    real_pattern = '[0-9]*\\.[0-9]+'
-    comment_pattern = '//[^\n]*|/\\*.*?\\*/'
+    real_pattern = r'\d+(?:\.\d+)*'
+    comment_pattern = r"//.*"
+    multiline_comment_pattern = r"/\*.*?\*/"
     operator_pattern = '|'.join(re.escape(op) for op in operators)
     punctuation_pattern = '|'.join(re.escape(p) for p in punctuations)
+    ignore_pattern = r'[@%&]'
 
-    # Combine all patterns into a single regex
-    pattern = '|'.join(['({})'.format(p) for p in [keyword_pattern, id_pattern, int_pattern,
-                       real_pattern, comment_pattern, operator_pattern, punctuation_pattern]])
+    left_multiline_comment_pattern = r"/\*"
+    right_multiline_comment_pattern = r"\*/"
+
+    pattern = '|'.join(['({})'.format(p) for p in [keyword_pattern,  id_pattern,  real_pattern,  int_pattern,
+                                                   comment_pattern,  multiline_comment_pattern,
+                                                   punctuation_pattern,  left_multiline_comment_pattern,
+                                                   right_multiline_comment_pattern,  operator_pattern,  ignore_pattern]])
     regex = re.compile(pattern)
 
-    # Start tokenizing
+    tokens = []
+    errors = []
+
     row, col = 1, 1
+    inside_multiline_comment = False
     for line in code.split('\n'):
+        temp_tokens = []
+        temp_errors = []
         for match in regex.finditer(line):
             lexeme = match.group()
-            if match.group(1):  # Keyword
-                token_type = 'KEYWORD'
-            elif match.group(2):  # Identifier
-                token_type = 'ID'
-            elif match.group(3):  # Integer number
-                token_type = 'NUMBER'
-            elif match.group(4):  # Real number
-                token_type = 'NUMBER'
-            elif match.group(5):  # Comment
-                continue  # Ignore comments
-            elif match.group(6):  # Operator
+
+            if '/*' in lexeme:
+                inside_multiline_comment = True
+                continue
+            if inside_multiline_comment:
+                if '*/' in lexeme:
+                    inside_multiline_comment = False
+                continue
+
+            if lexeme in operators:
                 token_type = 'OPERATOR'
-            elif match.group(7):  # Punctuation
+
+            elif match.group(1):
+                token_type = 'KEYWORD'
+
+            elif match.group(2):
+                if not validName(lexeme):
+                    token_type = 'INVALID_IDENTIFIER'
+                    temp_errors.append({
+                        'message': '{} is an invalid identifier'.format(lexeme),
+                        'row': row,
+                        'col': col
+                    })
+                else:
+                    token_type = 'ID'
+
+            elif match.group(3):
+                if '.' in lexeme:
+                    if validNumber(lexeme):
+                        token_type = 'FLOAT'
+                    else:
+                        token_type = 'INVALID_FLOAT'
+                        temp_errors.append({
+                            'message': '{} is not a valid float'.format(lexeme),
+                            'row': row,
+                            'col': col
+                        })
+                else:
+                    token_type = 'INTEGER'
+            elif match.group(4):
+                token_type = 'INTEGER'
+
+            elif match.group(5):
+                continue
+
+            elif match.group(6):
+                token_type = 'OPERATOR'
+
+            elif match.group(7):
                 token_type = 'PUNCTUATION'
-            else:  # Invalid token
-                errors.append({'lexeme': lexeme, 'row': row, 'col': col})
 
-            token = {'type': token_type,
-                     'lexeme': lexeme, 'row': row, 'col': col}
-            tokens.append(token)
+            elif re.match(r'[@%&]', lexeme):
+                token_type = 'INVALID_SYMBOL'
+                temp_errors.append({
+                    'message': '{} is an invalid symbol'.format(lexeme),
+                    'row': row,
+                    'col': col
+                })
 
-            # Update row and col
+            else:
+                continue
+
+            if token_type not in ['INVALID_IDENTIFIER', 'INVALID_FLOAT', 'INVALID_SYMBOL']:
+                token = {'type': token_type,
+                         'lexeme': lexeme, 'row': row, 'col': col}
+                temp_tokens.append(token)
             col += len(lexeme)
+
+        tokens.extend(temp_tokens)
+        errors.extend(temp_errors)
+
+        if inside_multiline_comment:
+            message = 'Expected end of multiline comment'
+            temp_errors.append({'message': message, 'row': row, 'col': col})
+
         row += 1
         col = 1
 
     return tokens, errors
+
+
+def validName(name):
+    pattern = '^[a-zA-Z][a-zA-Z0-9]*$'
+    if re.match(pattern, name):
+        return True
+    else:
+        return False
+
+
+def validNumber(expresion):
+    try:
+        float(expresion)
+        return True
+    except ValueError:
+        return False
 
 
 if len(sys.argv) > 1:
@@ -76,7 +152,6 @@ if len(sys.argv) > 1:
     with open(f'{sys.argv[1].split(".")[0]}_lexical_errors.txt', 'w') as f:
         f.write(error_text)
     print(output_text)
-    print(error_text)
+    print(f'errors -> {error_text}')
 else:
     print('Proporcione el archivo a analizar ...')
-
